6d166b7b4f6f4554ba0f3d66dbd3d26bb03bb594
johnnyinthesoutheast281 <trinm2801@gmail.com>
2022-07-18 04:23:48 +0700

    Add problem 27: Remove Element

---
+++ Easy/removeElement.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int i = 0;
+        while (i < nums.size()){
+            if (nums[i] == val){
+                for (int j = i; j < nums.size() - 1; ++j){
+                    nums[j] = nums[j + 1];
+                }
+                nums.pop_back();
+            }
+            else{
+                i++;
+            }
+        }
+        int result = nums.size();
+        return result;
+    }
+};
