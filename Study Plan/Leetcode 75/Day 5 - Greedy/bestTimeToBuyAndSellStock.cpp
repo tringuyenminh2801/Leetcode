3bb94b291bddf847bf7404048572bd05ca410f10
johnnyinthesoutheast281 <trinm2801@gmail.com>
2022-07-27 02:21:15 +0700

    Test commit

--- Study Plan/Leetcode 75/Day 5 - Greedy/bestTimeToBuyAndSellStock.cpp
+++ Study Plan/Leetcode 75/Day 5 - Greedy/bestTimeToBuyAndSellStock.cpp
@@ -18,8 +18,8 @@ public:
 };
 
 int main(){
-    vector<int> prices = {7,1,5,3,6,4};
     Solution s;
+    vector<int> prices = {7,1,5,3,6,4};
     cout << s.maxProfit(prices) << endl;
     return 0;
 }
